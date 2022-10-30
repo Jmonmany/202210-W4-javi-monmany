b399bfef0744eab52437708ff5bab3a65a8e633e
Javi <monmany1992@gmail.com>
2022-10-30 11:34:24 +0100

    Added first is statement

--- module.js
+++ module.js
@@ -1,3 +1,14 @@
 export function strictEquals(a, b) {
-    console.log('RESULT: ', Object.is(a,b) )
+    let result = Object.is(a, b);
+    if (Number.isNaN(a) & Number.isNaN(b)) {
+        result = false; 
+        console.log('RESULT: ', result);
+        return;
+    }
+    debugger
+    if () {
+        
+    }
+    
+    console.log('RESULT: ', result);
 }
