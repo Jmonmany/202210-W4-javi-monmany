b14f7991868d9d4047b4d2b0f51a91862d16ed64
Javier Monmany <97878775+Jmonmany@users.noreply.github.com>
2022-11-03 18:33:37 +0100

    Update module.js

--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 export default function strictEquals(a, b) {
     const result = Object.is(a, b);
-    if (Number.isNaN(a) & Number.isNaN(b)) {
+    if (Number.isNaN(a) && Number.isNaN(b)) {
         return !result;
     }
     if (Object.is(-Infinity, 1 / a) || Object.is(-Infinity, 1 / b)) {
